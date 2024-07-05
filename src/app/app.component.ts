fd5eded49e6c5f137572aa3b312b9b3a61de45dd
Anas Abbas <anasabbasmalik88@gmail.com>
2024-07-05 15:30:46 +0500

    Working with the Components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./Components/header/header.component"
+import { TasksComponent } from './Components/tasks/tasks.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,HeaderComponent,TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angIntro';
+  
 }
